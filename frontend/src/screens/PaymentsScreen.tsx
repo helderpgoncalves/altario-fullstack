3b3444e705e212ee1c397261709a6ff98aaff37b
Hélder Gonçalves <helderpimentaxc@gmail.com>
2024-10-19 18:41:38 +0100

    feat(frontend): Add current code display to PaymentsScreen

--- frontend/src/screens/PaymentsScreen.tsx
+++ frontend/src/screens/PaymentsScreen.tsx
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useGridContext } from "../contexts/GridContext";
 
 const PaymentsScreen: React.FC = () => {
   const { gridData } = useGridContext();
-  const [payments, setPayments] = useState([]);
-
-  useEffect(() => {
-    // TODO: Fetch payments from API
-  }, []);
 
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className="max-w-4xl mx-auto p-4">
       <h2 className="text-2xl font-bold mb-4">Current Code: {gridData.code}</h2>
-      {/* TODO: Add payment form */}
-      {/* TODO: Add payments list */}
+      {/* Payment form and list will be added here */}
     </div>
   );
 };
